b13257349cc5d928c37117116af06996f68e5d17
João Carlos de Farias Almeida <estefanecortes@gmail.com>
2021-07-15 19:09:06 -0300

    add Link

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -1,27 +1,29 @@
 import './App.css'
 import Login from './components/Login'
 import Profile from './components/Profile'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      <nav>
-        <ul>
-          <li>
-            <a href="/profile">Profile</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Router>
+      <Router>
+        <Route path="/" exact>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </Route>
+        <main>
           <Route path="/login" component={Login} />
           <Route path="/profile" component={Profile} />
-        </Router>
-      </main>
+        </main>
+      </Router>
     </div>
   )
 }
--- classe-01/src/components/VoltarHome/index.js
+++ classe-01/src/components/VoltarHome/index.js
@@ -1,5 +1,9 @@
 import './style.css'
-
+import { Link } from 'react-router-dom'
 export default function VoltarHome() {
-  return <button className="voltar-home"> ↩ Página principal</button>
+  return (
+    <Link to="/">
+      <button className="voltar-home">↩ Página principal</button>
+    </Link>
+  )
 }
